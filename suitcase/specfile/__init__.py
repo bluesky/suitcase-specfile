e4e383623236677844228ed8d06014134aeee2b2
Dan Allan <dallan@bnl.gov>
2020-02-10 19:21:31 -0500

    Do not mutate input data.
    Fixes a regression in #10

--- suitcase/specfile/__init__.py
+++ suitcase/specfile/__init__.py
@@ -531,6 +531,7 @@ class Serializer(event_model.DocumentRouter):
 
     def stop(self, doc):
         msg = '\n'
+        doc = doc.copy()
         doc.setdefault('reason', 'No reason recorded.')
         if doc['exit_status'] != 'success':
             msg += ('#C Run exited with status: {exit_status}. Reason: '
